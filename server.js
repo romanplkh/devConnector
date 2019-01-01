38f593373101a575de5f487b83a1aeff3b298399
Roman <pelikh.roman@gmail.com>
2019-01-01 18:57:58 -0400

    add profile validation and routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 //********************************BODY PARSER */
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,9 +22,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("HELLO");
-});
+//PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //****************************************** */Use Routes
 app.use("/api/users/", users);
