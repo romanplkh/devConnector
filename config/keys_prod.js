ec2d4709b936215bca6b97b7ab7d0809cfb6d30f
Roman <pelikh.roman@gmail.com>
2019-01-06 00:12:23 -0400

    beta deploy 1

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  tokenSecret: process.env.SECRET_KEY
+  tokenSecret: process.env.SECRET
 };
--- server.js
+++ server.js
@@ -36,10 +36,11 @@ app.use("/api/posts/", posts);
 
 //SERVE STATIC ASSEST FOR REACT IN PRODUCTION ONLY
 if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("cleint/build"));
+  // Set static folder
+  app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "cleint", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
